719988c587443f73d9ce4a58e6d4adba4e877220
zyachel <aricla@protonmail.com>
2022-06-08 00:30:43 +0530

    feat: add theme sensitive theme-color meta tags
    adds a nice touch to overall look of the webpage

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,10 @@
 //                      CONSTANTS
 ////////////////////////////////////////////////////////
 const rootEl = document.documentElement;
+const headerEl = document.querySelector('.header');
+const [metaThemeEl, tempMetaThemeEl] = document.querySelectorAll(
+  'meta[name="theme-color"]'
+);
 const btnTheme = document.querySelector('.theme-changer');
 
 ////////////////////////////////////////////////////////
@@ -23,8 +27,10 @@ const localStorageAccessible = !!typeof Storage;
 btnTheme.addEventListener('click', () => {
   const curTheme = rootEl.getAttribute('theme') || 'light';
   const themeToSet = curTheme === 'light' ? 'dark' : 'light';
+  const colorToAdd = window.getComputedStyle(headerEl).backgroundColor;
   setTheme(themeToSet);
-
+  // changes the meta theme-color tag to match the header color
+  metaThemeEl.setAttribute('content', colorToAdd);
   // only setting the value in localStoage if it's actually accessible
   if (localStorageAccessible) localStorage.setItem('theme', themeToSet);
 });
@@ -35,6 +41,9 @@ btnTheme.addEventListener('click', () => {
 (() => {
   // setting this attr on root to not render some css styles
   rootEl.setAttribute('js-enabled', '');
+  // removing duplicate meta theme color(it's initially for those who haven't enabled js)
+  metaThemeEl.removeAttribute('media');
+  tempMetaThemeEl.remove();
   // applying theme preferences in case they exist
   if (browserPrefersDarkTheme) setTheme('dark');
   else if (userPrefersTheme) setTheme(userPrefersTheme);
