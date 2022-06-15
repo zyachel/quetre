ad014480ea648ec69a5d2ab3abbe8f133bd7a0c7
zyachel <aricla@protonmail.com>
2022-06-15 22:09:07 +0530

    fix: change the code so as to compress static resources as well

--- app.js
+++ app.js
@@ -14,23 +14,10 @@ import AppError from './utils/AppError.js';
 ////////////////////////////////////////////////////////
 //            CREATING AND CONFIGURING APP
 ////////////////////////////////////////////////////////
+// 0. CREATING APP
 const app = express();
 
-app.set('view engine', 'pug');
-const pathToViews = fileURLToPath(new URL('./views/pug', import.meta.url));
-app.set('views', pathToViews);
-const pathToPublicDirectory = fileURLToPath(
-  new URL('./public', import.meta.url)
-);
-app.use(
-  express.static(pathToPublicDirectory, {
-    maxAge: process.env.CACHE_PERIOD || '1h',
-  })
-);
-
-////////////////////////////////////////////////////////
-//                   MIDDLEWARES
-////////////////////////////////////////////////////////
+// 1. IMPORTANT MIDDLWARES
 app.use(compression()); // compressing responses
 app.use(
   helmet({
@@ -43,8 +30,22 @@ app.use(
     crossOriginEmbedderPolicy: false,
   })
 ); // using sane headers on response
-if (process.env.NODE_ENV === 'development') app.use(morgan('dev')); // for logging during development
 
+// 2. SETTING VIEW ENGINE AND PATH TO STATIC ASSETS
+app.set('view engine', 'pug');
+const pathToViews = fileURLToPath(new URL('./views/pug', import.meta.url));
+app.set('views', pathToViews);
+const pathToPublicDirectory = fileURLToPath(
+  new URL('./public', import.meta.url)
+);
+app.use(
+  express.static(pathToPublicDirectory, {
+    maxAge: process.env.CACHE_PERIOD || '1h',
+  })
+);
+
+// 3. MISC MIDDLEWARES
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev')); // for logging during development
 // middleware to add baseUrl to req object
 app.use((req, res, next) => {
   req.urlObj = new URL(
@@ -53,7 +54,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// main middlewares to handle routes
+// 4. MIDDLWARES FOR ROUTES
 app.use('/', viewRouter);
 app.use('/api/v1/', apiRouter);
 
